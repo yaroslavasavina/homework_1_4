1efbd44daa277ec063acc7b5f2497ef7b618a765
yaroslavasavina <savina.yaroslava.igorevna@gmail.com>
2022-07-20 13:53:18 +0300

    homework_1_4_correct

--- src/Main.java
+++ src/Main.java
@@ -7,34 +7,33 @@ public class Main {
             System.out.print(" " + i);
             i ++;
         }{
-            System.out.println("\n");
+            System.out.println();
         }
 
         for (i =10; i>=1; i--){
             System.out.print(" " + i);
         } {
-            System.out.println("\n");
+            System.out.println();
         }
 
         // Task 2
-        int friday = 2;
-        for (; friday<=31;friday +=7 ) {
+
+        for (int friday = 2; friday<=31;friday +=7 ) {
             System.out.println("Сегодня пятница " + friday + "-е число. Пора сдавать отчет.");
         }{
-            System.out.println("\n");
+            System.out.println();
         }
 
         // Task 3
         int currentYear = 2022;
         int startYear = currentYear - 200;
         int endYear = currentYear + 100;
-        for (currentYear = startYear;currentYear<= endYear; currentYear+=79) {
-            if (currentYear<2022)
-            System.out.println("В " + currentYear + " году пролетала комета.");
-           else if (currentYear>= 2022)
-             System.out.println("В " + currentYear + " году пролетит комета.");
+        for (int year = startYear; year<= endYear; year ++) {
+            if (year%79 == 0 && year<2022)
+            System.out.println("В " + year + " году пролетала комета.");
+           else if (year%79 ==0 && year>= 2022)
+             System.out.println("В " + year + " году пролетит комета.");
 
         }
-
     }
 }
